c6b5b492a4d052a47da08444f5a9a7d7328f811d
fjalafoo <fjalafoo@gmail.com>
2022-12-02 23:11:32 +0300

    updates

--- travelist/main_app/views.py
+++ travelist/main_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+import uuid
+import boto3
 
 # Create your views here.
+
+# Define the home view
+def home(request):
+  return render(request, 'home.html')
+
+
+# Define the about view
+def about(request):
+  return render(request, 'about.html')
