1ddb6e0c565f1e7a1accd3fae450aa47ea9aa557
fjalafoo <fjalafoo@gmail.com>
2022-12-05 00:15:25 +0300

    registered models in admin.py

--- travelist/main_app/admin.py
+++ travelist/main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Country
+from .models import Bucket
+from .models import Review
+
 
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(Bucket)
+admin.site.register(Review)
