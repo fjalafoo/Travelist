7c2103bad301e2b6d10e030a64259c19f6f6af59
fjalafoo <fjalafoo@gmail.com>
2022-12-06 22:56:49 +0300

    migrations

--- travelist/main_app/models.py
+++ travelist/main_app/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 # Country model
 class Country(models.Model):
+
     name = models.CharField(max_length=100)
     flag = models.TextField(max_length=500)
     language = models.CharField(max_length=20)
     currency = models.CharField(max_length=30)
+
+    # Add the foreign key linking to a user instance
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
 
 
 # Bucket model
@@ -17,15 +23,21 @@ class Bucket(models.Model):
     location = models.TextField(max_length=500)
     budget = models.CharField(max_length=30)
     description = models.TextField(max_length=400)
-     # Create a country_id FK
+
+    # Create a country_id FK
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+
 
 
 
 # Review model
 class Review(models.Model):
     description = models.TextField(max_length=400)
+    # Create a bucket_id FK
     bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE)
+
+   
 
 
 
